066ac5897cba9ec698e0d1f02d0e0b015369ad89
Yotam Shvartsun <yotam.shvartsun@gmail.com>
2019-10-19 15:36:35 +0300

    files can be viewed more then one time added database

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
-/** TODO:
- * let user input number of views
- * tests
- * for pictures: display as an html inline
- * style (?)
- * babel config + npm start script
-*/
-
 import express from 'express'
 import multer from 'multer';
 import {exists, unlink} from 'fs';
 import {join, resolve} from 'path';
 import { randomBytes } from 'crypto';
+import bodyParser from 'body-parser';
+
 let sqlite = require('sqlite3');
 
 let storage = multer.diskStorage({
@@ -20,7 +14,7 @@ let storage = multer.diskStorage({
     filename: (req, file, cb)=>{
         console.log(file.originalname);
         let originalTypeEnd = file.originalname.split('.').pop();
-        cb(null, `${randomBytes(16).toString('base64')}.${originalTypeEnd}`);
+        cb(null, `${randomBytes(16).toString('hex')}.${originalTypeEnd}`);
     }
 });
 
@@ -28,6 +22,8 @@ const filenameRegEx = /[^\\/]+\.[^\\/]+$/;
 const __dirname = resolve();
 let db = new sqlite.Database(join(__dirname, 'db.sqlite'));
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static('static'));
 app.set('views', join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -38,8 +34,7 @@ app.get('/', (req, res)=>{
 });
 
 app.get('/getfile', (req, res)=>{
-    let fileName = req.query.filename;
-    console.log(fileName);
+    let fileName = req.query.file;
     
     if(!fileName.match(filenameRegEx)){
         // error: invalid file name!
@@ -47,27 +42,47 @@ app.get('/getfile', (req, res)=>{
         return;
     }
     let maybePath = join(__dirname, '/uploads', fileName);
-    console.log(`path given: ${maybePath}`)
     exists(maybePath, (exists)=>{
         if(!exists){
             res.render('error', {err: 'not found'});
             return;
         }
-        res.sendFile(maybePath).then((re, rj)=>{
-            unlink(maybePath);
-        });        
-    });
+        res.sendFile(maybePath, (err)=>{
+            if(err) {
+                next(err);
+            }else{
+                db.all('select * from files where filename=?',
+                fileName,
+                (err, rows)=>{
+                    let fileRow = rows[0];
+                    if(fileRow.maxViews == 1){
+                        unlink(maybePath, ()=>{
+                            db.run('delete from files where filename=?', fileName);
+                        });
+                    }
+                    else{
+                        db.run('update files set maxViews=? where filename=?', fileRow.maxViews - 1, fileName);
+                    }
+                });
+            }
+        });
+});
 });
 
 app.post('/fileupload', upload.single('file'), (req, res)=>{
-    console.log(req.file);
-    res.render('upload', {url: `/getfile/${req.file.filename}`});
+    let maxViews = 1;
+    if(req.body.maxViews){
+        maxViews = req.body.maxViews;
+    }
+    db.run('insert into files(filename, maxViews) values(?,?)', req.file.filename, maxViews);
+    res.render('upload', {url: `/getfile?file=${req.file.filename}`});
 });
 
 let server = app.listen(8080, ()=>{
     db.run("create table if not exists files(filename text primery key, maxViews integer);")
     console.log('on 8080!');
 });
+
 let shutdown = () =>{
     console.log()
     console.group('Shutdown:');
